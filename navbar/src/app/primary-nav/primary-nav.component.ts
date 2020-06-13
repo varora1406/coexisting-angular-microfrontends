26fe53f6a27a7ecf9c033c844d8c9367d726334a
Vaibhav <varora@catalystone.com>
2020-06-14 00:41:31 +0530

    feat: add infinite issue creator button

--- navbar/src/app/primary-nav/primary-nav.component.ts
+++ navbar/src/app/primary-nav/primary-nav.component.ts
@@ -1,25 +1,25 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Router } from '@angular/router';
-import { Subject, Subscription } from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Router } from "@angular/router";
+import { Subject, Subscription } from "rxjs";
+import { debounceTime } from "rxjs/operators";
 
 @Component({
-  selector: 'navbar-primary-nav',
-  templateUrl: './primary-nav.component.html',
-  styleUrls: ['./primary-nav.component.css']
+  selector: "navbar-primary-nav",
+  templateUrl: "./primary-nav.component.html",
+  styleUrls: ["./primary-nav.component.css"],
 })
 export class PrimaryNavComponent implements OnInit, OnDestroy {
   private readonly routeSubject = new Subject();
   private routeSubjectSubscription: Subscription;
 
-  constructor(private readonly router: Router) { }
+  constructor(private readonly router: Router) {}
 
   ngOnInit() {
     const routeCallback = (path) => {
       this.router.navigate([path], {
         queryParams: {
-          random: Math.random().toString(36).substring(7)
-        }
+          random: Math.random().toString(36).substring(7),
+        },
       });
     };
 
@@ -33,8 +33,8 @@ export class PrimaryNavComponent implements OnInit, OnDestroy {
     // Infinite URL loop doesn't work fine
     this.router.navigate([path], {
       queryParams: {
-        random: Math.random().toString(36).substring(7)
-      }
+        random: Math.random().toString(36).substring(7),
+      },
     });
   }
 
